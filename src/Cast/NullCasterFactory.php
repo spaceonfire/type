bf2d465a3396cc24e0d358b7b383552a559c5581
Constantine Karnaukhov <genteelknight@gmail.com>
2021-10-03 14:12:40 +0000

    feat(type): add type caster factory

---
+++ src/Cast/NullCasterFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spaceonfire\Type\Cast;
+
+use spaceonfire\Type\BuiltinType;
+use spaceonfire\Type\TypeInterface;
+
+final class NullCasterFactory implements CasterFactoryInterface
+{
+    private string $accept;
+
+    /**
+     * @phpstan-param NullCaster::ACCEPT_* $accept
+     */
+    public function __construct(string $accept = NullCaster::ACCEPT_EMPTY)
+    {
+        $this->accept = $accept;
+    }
+
+    public function make(TypeInterface $type): ?CasterInterface
+    {
+        if (BuiltinType::null() === $type) {
+            return new NullCaster($this->accept);
+        }
+
+        return null;
+    }
+}
